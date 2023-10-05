400d5cc56c318ba6ced1931df822cb5bd184f385
Iryna Howarth <irynahowarth@gmail.com>
2023-10-05 14:21:36 +0100

    added filter by country name and name/region

--- src/pages/Countries.jsx
+++ src/pages/Countries.jsx
@@ -5,32 +5,46 @@ import CountryCard from "../components/CountryCard";
 export default function Countries() {
   const [countries, setCountries] = React.useState([]);
   const [filterRegion, setFilterRegion] = React.useState("");
+  const [filterName, setFilterName] = React.useState("");
 
   React.useEffect(() => {
-    const urlFetch = filterRegion
-      ? `https://restcountries.com/v3.1/region/${filterRegion}`
-      : "https://restcountries.com/v3.1/all";
+    const urlFetch =
+      filterRegion && !filterName
+        ? `https://restcountries.com/v3.1/region/${filterRegion}`
+        : filterName
+        ? `https://restcountries.com/v3.1/name/${filterName}`
+        : `https://restcountries.com/v3.1/all`;
     fetch(urlFetch)
       .then((res) => res.json())
       .then((data) => setCountries(data));
-  }, [filterRegion]);
+  }, [filterName, filterRegion]);
+
+  function renderCountry(arr) {
+    return arr.map((country) => {
+      return (
+        <Link key={country.cca3} to={`/${country.cca3.toLowerCase()}`}>
+          <CountryCard
+            name={country.name.official}
+            population={country.population}
+            region={country.region}
+            capital={country.capital}
+            image={country.flags.svg}
+          />
+        </Link>
+      );
+    });
+  }
 
   const countryList =
-    (countries.length > 0 &&
-      countries.map((country) => {
-        return (
-          <Link key={country.cca3} to={`/${country.cca3.toLowerCase()}`}>
-            <CountryCard
-              name={country.name.official}
-              population={country.population}
-              region={country.region}
-              capital={country.capital}
-              image={country.flags.svg}
-            />
-          </Link>
-        );
-      })) ||
-    "";
+    countries.length > 0
+      ? filterName && filterRegion
+        ? renderCountry(
+            countries.filter((country) => {
+              return country.region.toLowerCase() === filterRegion;
+            })
+          )
+        : renderCountry(countries)
+      : "No data availible";
 
   return (
     <>
@@ -39,10 +53,17 @@ export default function Countries() {
         style={{ display: "block" }}
         onSubmit={(event) => event.preventDefault()}
       >
-        <input placeholder="Search for a country..." />
+        <input
+          placeholder="Search for a country..."
+          id="name-filter"
+          value={filterName}
+          onChange={(event) => {
+            setFilterName(event.target.value);
+          }}
+        />
         <select
           name="region"
-          id="region-select"
+          id="region-filter"
           value={filterRegion}
           onChange={(event) => {
             setFilterRegion(event.target.value);
@@ -50,7 +71,7 @@ export default function Countries() {
         >
           <option value="">Filter by Region</option>
           <option value="africa">Africa</option>
-          <option value="america">America</option>
+          <option value="americas">Americas</option>
           <option value="asia">Asia</option>
           <option value="europe">Europe</option>
           <option value="oceania">Oceania</option>
