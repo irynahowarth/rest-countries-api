5348302917257dc77946f56be958837d34f34c2c
Iryna Howarth <irynahowarth@gmail.com>
2023-10-05 12:22:33 +0100

    added functionality Filter by region to Countries page.

--- src/pages/Countries.jsx
+++ src/pages/Countries.jsx
@@ -4,16 +4,20 @@ import CountryCard from "../components/CountryCard";
 
 export default function Countries() {
   const [countries, setCountries] = React.useState([]);
+  const [filterRegion, setFilterRegion] = React.useState("");
 
   React.useEffect(() => {
-    fetch("https://restcountries.com/v3.1/all")
+    const urlFetch = filterRegion
+      ? `https://restcountries.com/v3.1/region/${filterRegion}`
+      : "https://restcountries.com/v3.1/all";
+    fetch(urlFetch)
       .then((res) => res.json())
       .then((data) => setCountries(data));
-  }, []);
+  }, [filterRegion]);
+
   const countryList =
     (countries.length > 0 &&
       countries.map((country) => {
-        // country.name.common
         return (
           <Link key={country.cca3} to={`/${country.cca3.toLowerCase()}`}>
             <CountryCard
@@ -31,9 +35,19 @@ export default function Countries() {
   return (
     <>
       <h1>List of Countries here</h1>
-      <form style={{ display: "none" }}>
+      <form
+        style={{ display: "block" }}
+        onSubmit={(event) => event.preventDefault()}
+      >
         <input placeholder="Search for a country..." />
-        <select name="region" id="region">
+        <select
+          name="region"
+          id="region-select"
+          value={filterRegion}
+          onChange={(event) => {
+            setFilterRegion(event.target.value);
+          }}
+        >
           <option value="">Filter by Region</option>
           <option value="africa">Africa</option>
           <option value="america">America</option>
